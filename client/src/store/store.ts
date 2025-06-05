c1ebd6b8653148b9db741cc37283721d76441865
Montoya086 <84055444+Montoya086@users.noreply.github.com>
2025-06-05 00:14:04 -0600

    feat: session persist

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -3,17 +3,30 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { apiSlice } from './api/baseApi-slice';
 import appStateReducer from './slices/appState-slice';
 import { authMiddleware } from './middleware/authMiddleware';
+import { persistStore, persistReducer } from 'redux-persist';
+import storageSession from 'redux-persist/lib/storage/session';
+
+const persistConfig = {
+  key: 'root',
+  storage: storageSession,
+};
+
+const persistedReducer = persistReducer(persistConfig, appStateReducer);
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    appState: appStateReducer,
+    appState: persistedReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
+    getDefaultMiddleware({
+      serializableCheck: false,
+    })
       .concat(apiSlice.middleware)
       .concat(authMiddleware),
 });
+
+export const persistor = persistStore(store);
 
 setupListeners(store.dispatch);
 
